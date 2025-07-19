970d14e4d4afdbeb889e0a332948e3410add5929
Aark <itsaark@users.noreply.github.com>
2025-07-18 17:16:32 -0700

    Initial Commit

---
+++ lore/loreApp.swift
@@ -0,0 +1,17 @@
+//
+//  loreApp.swift
+//  lore
+//
+//  Created by Aark Koduru on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct loreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ loreTests/loreTests.swift
@@ -0,0 +1,17 @@
+//
+//  loreTests.swift
+//  loreTests
+//
+//  Created by Aark Koduru on 7/18/25.
+//
+
+import Testing
+@testable import lore
+
+struct loreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
